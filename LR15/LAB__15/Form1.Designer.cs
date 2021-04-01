0e57f5f271d2911b3b8a110b478f7c6bc4e86c82
PilipenkoEugene <70794926+PilipenkoEugene@users.noreply.github.com>
2021-04-02 00:00:19 +0300

    Add files via upload

---
+++ LR15/LAB__15/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnChooseFont = new System.Windows.Forms.Button();
+            this.panelChooseColor = new System.Windows.Forms.Panel();
+            this.txtChooseFont = new System.Windows.Forms.TextBox();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.txtChooseFolder = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnChooseColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseColor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChooseColor.Location = new System.Drawing.Point(27, 28);
+            this.btnChooseColor.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(164, 42);
+            this.btnChooseColor.TabIndex = 0;
+            this.btnChooseColor.Text = "Выбор цвета ";
+            this.btnChooseColor.UseVisualStyleBackColor = false;
+            this.btnChooseColor.Click += new System.EventHandler(this.BtnChooseColor_Click);
+            // 
+            // btnChooseFont
+            // 
+            this.btnChooseFont.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnChooseFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseFont.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChooseFont.Location = new System.Drawing.Point(27, 83);
+            this.btnChooseFont.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChooseFont.Name = "btnChooseFont";
+            this.btnChooseFont.Size = new System.Drawing.Size(164, 42);
+            this.btnChooseFont.TabIndex = 1;
+            this.btnChooseFont.Text = "Выбор шрифта";
+            this.btnChooseFont.UseVisualStyleBackColor = false;
+            this.btnChooseFont.Click += new System.EventHandler(this.BtnChooseFont_Click);
+            // 
+            // panelChooseColor
+            // 
+            this.panelChooseColor.Location = new System.Drawing.Point(208, 28);
+            this.panelChooseColor.Margin = new System.Windows.Forms.Padding(4);
+            this.panelChooseColor.Name = "panelChooseColor";
+            this.panelChooseColor.Size = new System.Drawing.Size(165, 42);
+            this.panelChooseColor.TabIndex = 2;
+            // 
+            // txtChooseFont
+            // 
+            this.txtChooseFont.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtChooseFont.Location = new System.Drawing.Point(208, 95);
+            this.txtChooseFont.Margin = new System.Windows.Forms.Padding(4);
+            this.txtChooseFont.Name = "txtChooseFont";
+            this.txtChooseFont.Size = new System.Drawing.Size(181, 20);
+            this.txtChooseFont.TabIndex = 3;
+            this.txtChooseFont.Text = "Образец шрифта";
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnChooseFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseFolder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChooseFolder.Location = new System.Drawing.Point(27, 135);
+            this.btnChooseFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(164, 42);
+            this.btnChooseFolder.TabIndex = 6;
+            this.btnChooseFolder.Text = "Выбор каталога";
+            this.btnChooseFolder.UseVisualStyleBackColor = false;
+            this.btnChooseFolder.Click += new System.EventHandler(this.BtnChooseFolder_Click);
+            // 
+            // txtChooseFolder
+            // 
+            this.txtChooseFolder.Location = new System.Drawing.Point(208, 147);
+            this.txtChooseFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.txtChooseFolder.Name = "txtChooseFolder";
+            this.txtChooseFolder.Size = new System.Drawing.Size(165, 20);
+            this.txtChooseFolder.TabIndex = 7;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(377, 386);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 30);
+            this.labelResult.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(208, 147);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 42);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Переменная: Х";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(271, 61);
+            this.textBoxX.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(131, 24);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Кол-во слагаемых: N ";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(271, 111);
+            this.textBoxN.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(131, 24);
+            this.textBoxN.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxN);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxX);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 60);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(438, 210);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnChooseFolder);
+            this.groupBox2.Controls.Add(this.btnChooseFont);
+            this.groupBox2.Controls.Add(this.txtChooseFolder);
+            this.groupBox2.Controls.Add(this.txtChooseFont);
+            this.groupBox2.Controls.Add(this.panelChooseColor);
+            this.groupBox2.Controls.Add(this.btnChooseColor);
+            this.groupBox2.Location = new System.Drawing.Point(469, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(405, 210);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(895, 461);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnChooseFont;
+        private System.Windows.Forms.Panel panelChooseColor;
+        private System.Windows.Forms.TextBox txtChooseFont;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.TextBox txtChooseFolder;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ LR15/LAB__15/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.ShowHelp = true;
+            dlg.Color = panelChooseColor.BackColor;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                panelChooseColor.BackColor = dlg.Color;
+                labelResult.BackColor = dlg.Color;
+            }
+        }
+
+        private void BtnChooseFont_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            dlg.ShowColor = true;
+            dlg.ShowHelp = true;
+            dlg.Font = txtChooseFont.Font;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtChooseFont.Font = dlg.Font;
+                labelResult.Font = dlg.Font;
+                txtChooseFont.ForeColor = dlg.Color;
+                labelResult.ForeColor = dlg.Color;
+            }
+        }
+
+        private void BtnChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.Description = "выберите папку для демонстрации работы диалога";
+            dlg.ShowNewFolderButton = true;
+            dlg.SelectedPath = Application.StartupPath;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtChooseFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        
+
+        private void Button1_Click(object sender, EventArgs e)
+        {            
+            double x = Convert.ToDouble(textBoxX.Text);
+            double n = Convert.ToDouble(textBoxN.Text);
+            
+
+            double result = 0;
+            int znam = 2;
+            if (n > 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    long zz = 1;
+                    for (int j = 1; j <= znam ; j++)
+                    {
+                        zz = zz * j;
+                    }
+                    znam++;
+                    if (i % 2 == 0)
+                        result += Math.Sin(Math.Pow(x, i + 1)) * Math.Pow(x, i + 2) / zz;
+                    else
+                        result += Math.Pow(x, i + 1) * Math.Sin(Math.Pow(x, i + 2)) / zz;
+                }
+
+                labelResult.Text = String.Format("Ответ = {0:0.00}", result);
+            }
+            else
+            {
+                MessageBox.Show("Введите допустимое n");
+            }
+
+            if (txtChooseFolder.Text != "")
+            {
+                string file = Path.Combine(txtChooseFolder.Text, "result.txt");
+                File.WriteAllText(file, Convert.ToString(result));
+            }
+        }
+    }
+}
